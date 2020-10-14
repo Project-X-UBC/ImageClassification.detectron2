0c499c0630efa42a84fc2e457b9c23a186786ae2
rdesc <rodrigue.de.schaetzen@gmail.com>
2020-10-14 23:44:09 +0000

    cleaning

--- train_net_builtin.py
+++ train_net_builtin.py
@@ -1,16 +1,13 @@
-'''
+"""
 @Copyright (c) tkianai All Rights Reserved.
 @Author         : tkianai
 @Github         : https://github.com/tkianai
 @Date           : 2020-04-26 14:02:06
 @FilePath       : /ImageCls.detectron2/train_net_builtin.py
-@Description    : 
-'''
+@Description    :
+"""
 
-import logging
 import os
-from collections import OrderedDict
-import torch
 
 import detectron2.utils.comm as comm
 from detectron2.checkpoint import DetectionCheckpointer
@@ -20,10 +17,9 @@ from detectron2.evaluation import (
     DatasetEvaluators,
     verify_results,
 )
-from detectron2.modeling import GeneralizedRCNNWithTTA
 
 from imgcls.config import get_cfg
-import imgcls.modeling
+import imgcls.modeling # need this import to initialize modeling package
 from imgcls.data import DatasetMapper
 from imgcls.evaluation.imagenet_evaluation import ImageNetEvaluator
 
@@ -53,29 +49,7 @@ class Trainer(DefaultTrainer):
         """
         if output_folder is None:
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
-        return ImageNetEvaluator(dataset_name, True, output_folder)  # TODO: most likely need to modify evaluator
-
-    # TODO: wont work for model
-    # TODO: modfiy
-    @classmethod
-    def test_with_TTA(cls, cfg, model):
-        # Test Time Augmentation explanation about it here
-        # https://towardsdatascience.com/test-time-augmentation-tta-and-how-to-perform-it-with-keras-4ac19b67fb4d
-        logger = logging.getLogger("detectron2.trainer")
-        # In the end of training, run an evaluation with TTA
-        # Only support some R-CNN models.
-        logger.info("Running inference with test-time augmentation ...")
-        model = GeneralizedRCNNWithTTA(cfg, model)
-        evaluators = [
-            cls.build_evaluator(
-                cfg, name, output_folder=os.path.join(
-                    cfg.OUTPUT_DIR, "inference_TTA")
-            )
-            for name in cfg.DATASETS.TEST
-        ]
-        res = cls.test(cfg, model, evaluators)
-        res = OrderedDict({k + "_TTA": v for k, v in res.items()})
-        return res
+        return ImageNetEvaluator(dataset_name, True, output_folder)
 
 
 def setup(args):
@@ -95,14 +69,10 @@ def main(args):
 
     if args.eval_only:
         model = Trainer.build_model(cfg)
-        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
-            cfg.MODEL.WEIGHTS, resume=args.resume
-        )
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(cfg.MODEL.WEIGHTS, resume=args.resume)
         res = Trainer.test(cfg, model)
-        if cfg.TEST.AUG.ENABLED:
-            res.update(Trainer.test_with_TTA(cfg, model))
         if comm.is_main_process():
-            verify_results(cfg, res)
+            verify_results(cfg, res)  # TEST.EXPECTED_RESULTS is an empty list
         return res
 
     """
@@ -111,10 +81,7 @@ def main(args):
     """
     trainer = Trainer(cfg)
     trainer.resume_or_load(resume=args.resume)
-    if cfg.TEST.AUG.ENABLED:
-        trainer.register_hooks(
-            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
-        )
+
     return trainer.train()
 
 
@@ -130,3 +97,4 @@ if __name__ == "__main__":
         dist_url="auto",
         args=(args,),
     )
+
